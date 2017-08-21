4725d194312b1e9d84488d562049c8801bf712a6
Aleksandra Samsonovitch <aleksandra@Aleksandras-MacBook-Pro.local>
2017-08-21 20:20:47 +0300

    fucked

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,35 @@
 //var app = angular.module('beerList', ['ui.router']);
-var app = angular.module('dankTickets', []);
+var app = angular.module('dankTickets', ['ui.router']);
+
+app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
+  $locationProvider.html5Mode(true);
+  $urlRouterProvider.otherwise('/home');
+  $stateProvider
+  .state('index', {
+      url: '/home',
+      templateUrl: '/templates/index.html',
+ 
+    })
+    .state('event', {
+      url: '/event',
+      templateUrl: '/templates/event.html',
+ 
+    })
+    .state('aboutus', {
+      url: '/about',
+      templateUrl: '/templates/about-us.html'
+    })
+    .state('createevent', {
+      url: '/createevent',
+      templateUrl: '/templates/createEvent.html'
+    })
+    .state('services', {
+      url: '/services',
+      templateUrl: '/templates/services.html'
+    })
+    .state('page-contact', {
+      url: '/page-contact',
+      templateUrl: '/templates/page-contact.html',
+    })
+ 
+});
