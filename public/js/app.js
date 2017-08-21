a1b2ac0208978a8fb08eeb59a2a0cb599134cc83
Arik92 <arik92@gmail.com>
2017-08-21 17:55:26 +0300

    final working version

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,16 @@
 //var app = angular.module('beerList', ['ui.router']);
-var app = angular.module('dankTickets', []);
+var app = angular.module('dankTickets', ['ui-router']);
+app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
+  $locationProvider.html5Mode(true);
+  $urlRouterProvider.otherwise('/home');
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '../index.html',
+      controller: 'homeCtrl'
+    })
+    .state('new', {
+      url: '/new',
+      templateUrl: '../createEvent.html'
+    })//new
+  });//app
