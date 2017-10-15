e16187bf7dce30c2b3d627b0a65134f8d5688f53
mbreslow56 <mbreslow56@gmail.com>
2017-10-15 14:06:02 +0300

    made a few small changes

--- public/js/controllers/userCtrl.js
+++ public/js/controllers/userCtrl.js
@@ -1,7 +1,9 @@
 app.controller('regCtrl', function($http) {
   this.regUser = function (regData) {
     console.log('form submitted');
-    $http.post('/users/users', this.regData);
+    $http.post('/users/users', this.regData).then(function(data) {
+      console.log(data);
+    });
   };
 });
 
