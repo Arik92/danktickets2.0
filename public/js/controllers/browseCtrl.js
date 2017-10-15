4ce4a8650eea4ead8121d655332b5cc8eb4c25d9
Arik92 <arik92@gmail.com>
2017-10-15 15:22:56 +0300

    image path WORKS!!

--- public/js/controllers/browseCtrl.js
+++ public/js/controllers/browseCtrl.js
@@ -3,7 +3,7 @@ app.controller('browseCtrl',['createService','$scope','$window', function(create
     console.log("All events: ",result);
     $scope.events = result;
     for (var i=0;i<$scope.events.length;i++) {
-      $scope.events[i].imgPath ='public/img/uploads/' + $scope.events[i].image;
+      $scope.events[i].imgPath ='/img/uploads/' + $scope.events[i].image;
       console.log("path is", $scope.events[i].imgPath);
     }//for
   }, function(err){
