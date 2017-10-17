07e483bd761d5883a21898e7373f6257e575f4fd
mbreslow56 <mbreslow56@gmail.com>
2017-10-18 01:46:35 +0300

    root scope is working and navbar shows when user logged in or out

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -46,10 +46,30 @@ module.exports = function (router) {
       }
     });
   }); 
+
+  router.use(function (req, res, next) {
+    var token = req.body.token || req.body.query || req.headers['x-access-token'];
+    if (token) {
+      // verify a token symmetric
+      jwt.verify(token, secret, function(err, decoded) {
+        if (err) {
+          res.json({ success: false, message: 'token invalid' });
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      res.json({ success: false, message: 'No token provided' });
+    }
+  });
+
+  router.post('/currentUser', function(req, res) {
+    res.send(req.decoded);
+  });
+
+
   return router;
 }
 
 
-jwt.sign({
-  data: 'foobar'
-}, 'secret', { expiresIn: '1h' });
