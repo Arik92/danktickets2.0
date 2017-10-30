30eb2934d9e944696b78c8a136d14dd456bea0b2
Arik92 <arik92@gmail.com>
2017-10-30 14:49:20 +0200

    before pull

--- public/js/services/orService.js
+++ public/js/services/orService.js
@@ -1,14 +1,51 @@
 app.factory('orService', function($http){
-  function postEvent() {
-  return $http.post('/event').then(function(result) {
-    console.log("photo saved and set!");
-    console.log(result.data);
+  var getOrganizersByUser = function(userId) {
+    return $http.get('/organizers/'+userId).then(function(result) {
+      console.log("result from organizer service service: ");
+      console.log(result.data);
+      return result.data;
+    }, function(error) {
+      console.error(error);
+      // throw (error);
+    }) // add promise
+  } // get all organizer profiles for a given user id
+  var getAllOrganizers = function() {
+    return $http.get('/organizers')
+      .then(function(response) {
+        return response.data;
+      }, function(err) {
+        console.error(err);
+      });
+  }// getting events by a certain publisher
+  var postOrganizer = function(organizer) {
+  return $http.post('/organizers', organizer).then(function(result) {
+    console.log("save profile result");
     return result.data;
   }, function(error) {
+    console.error(error);
     throw (error);
   }) // add promise
-}// post event
-  return {
-    postOrganizer: postOrganizer
-  };
+  }// post event
+  var updateOrganizer = function(organizer) {
+    return $http.put('/events/' + organizer._id, organizer).then(function(result) {
+      return result.data;
+    }, function(error) {
+      throw error;
+    })
+  }
+  var deleteOrganizer = function(id) {
+    return $http.delete('/events/' + id).then(function(result) {
+      console.log("organizer profile that was deleted: ", result);
+      return result.data; //??
+    }, function(error) {
+      throw (error);
+    }) //promise callbacks
+  } // delete organizer profile
+      return {
+        getAllOrganizers: getAllOrganizers,
+        getOrganizersByUser: getOrganizersByUser,
+        postOrganizer: postOrganizer,
+        updateOrganizer: updateOrganizer,
+        deleteOrganizer: deleteOrganizer
+    };
 });
