66c3688dac713a6209c6b0eaf0934f7bfd459934
mbreslow56 <mbreslow56@gmail.com>
2017-10-31 17:39:09 +0200

    fb seems to be working up to this point. 14:37 of https://www.youtube.com/watch?v=IcFtBJBXzWw&index=10&list=PL3vQyqzqjZ637sWpKvniMCxdqZhnMJC1d

--- passport/passport.js
+++ passport/passport.js
@@ -1,5 +1,45 @@
+var FacebookStrategy = require('passport-facebook').Strategy;
+var User             = require('../models/usermodel'); // Import User Model
+var session          = require('express-session'); // Import Express Session Package
 
+module.exports = function(app, passport) {
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: true, cookie: { secure: false } }));
 
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use(new FacebookStrategy({
+      clientID: '793514707495227',
+      clientSecret: '12d8dcfc4b9a3c728c0b38795a0b500b',
+      callbackURL: "http://localhost:8000/auth/facebook/callback",
+      profileFields: ['id', 'displayName', 'photos', 'email']
+    },
+    function(accessToken, refreshToken, profile, done) {
+      console.log(profile._json.email);
+      // User.findOrCreate(..., function(err, user) {
+      //   if (err) { return done(err); }
+      //   done(null, user);
+      // });
+      done(null, profile);
+    }
+  ));
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/signin' }));
+
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+  
+  return passport;
+}
 
 // var FacebookStrategy = require('passport-facebook').Strategy; // Import Passport-Facebook Package
 // var User             = require('../models/usermodel'); // Import User Model
