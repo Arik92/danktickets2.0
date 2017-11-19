cb545ef0f24e9ba0b391c81d70e2f307904e564c
Natti Katz <katzy687@gmail.com>
2017-11-19 18:59:35 +0200

    added ui-bootstrap dropdown, routes need to be double-checked. Maybe a tad more creative styling as well

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(passport.initialize());
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(express.static('bower_components'));
+app.use(express.static('ui-bootstrap-custom-build'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/events', eventRoutes);
