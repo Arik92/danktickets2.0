e26aff18696b1630144b68b6de08a76ed4ef04d9
Arik92 <arik92@gmail.com>
2017-11-21 12:41:09 +0200

    testing for errors

--- public/bundle.js
+++ public/bundle.js
@@ -290,7 +290,8 @@ $scope.updateEndHr = function() {
                     console.log("image name will be?", $scope.imageName);
                   //  publishEvent(); // call a function to submit the whole event
                 } else {
-                    $window.alert(resp.data.error_code);
+					console.log(resp.data);
+                    $window.alert("response is", resp.data);
                 }
             }, function (error) { //catch error
                 console.log('Error status: ' + error);
