a626cf6da70de8e16bfb260213346e6feb72b1ef
Arik92 <arik92@gmail.com>
2017-11-27 15:54:08 +0200

    fb self fix

--- models/passport.js
+++ models/passport.js
@@ -2,7 +2,8 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 var passport = require('passport');
 var User   = require('./usermodel');
 var jwt    = require('jsonwebtoken');
-
+//localhost:8000/users/facebook/callback
+// http://danktickets.herokuapp.com/users/facebook/callback
 passport.use(new FacebookStrategy({
       clientID: '793514707495227',
       clientSecret: '12d8dcfc4b9a3c728c0b38795a0b500b',
@@ -11,6 +12,7 @@ passport.use(new FacebookStrategy({
     },
     function(accessToken, refreshToken, profile, done) {
       //code to check database goes here
+	  console.log("profile data", profile);
       User.findOne({'socialId': profile.id}, function(err, user){
         if (err) {
           console.error(err);
