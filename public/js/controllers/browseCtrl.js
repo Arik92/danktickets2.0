35b02340ae1a133b0e2804385268db4269be09d3
Arik92 <arik92@gmail.com>
2017-11-30 18:20:05 +0200

    browseCtrl

--- public/js/controllers/browseCtrl.js
+++ public/js/controllers/browseCtrl.js
@@ -4,11 +4,11 @@ app.controller('browseCtrl',['createService','$scope','$window', function(create
     $scope.events = result;
     /*for (var i=0;i<$scope.events.length;i++) {
       $scope.events[i].imgPath ='/img/uploads/' + $scope.events[i].image;
-      console.log("location"+i+" :", $scope.events[i].location);*/
+      console.log("location"+i+" :", $scope.events[i].location);
       // $scope.events[i].starts = $scope.events[i].startTime.toLocaleTimeString();
       // console.log("starts at ", $scope.events[i].starts);
       // console.log("path is", $scope.events[i].imgPath);
-    }//for
+    }//for */
   }, function(err){
     throw (err)
   })//GET request route
