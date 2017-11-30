a61a9a7772a74313dd6a17836497563d0f306ea8
Arik92 <arik92@gmail.com>
2017-11-30 15:39:41 +0200

    cors again

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express     = require('express');
 var app         = express();
 var port        = process.env.PORT || '8000';
+var cors = require('cors');
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
 var bodyParser  = require('body-parser');
