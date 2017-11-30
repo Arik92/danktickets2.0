b2cc3254a2080a5b947a6fee1df6fabf2c9be871
Arik92 <arik92@gmail.com>
2017-11-30 19:45:36 +0200

    options

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/organizers', organizerRoutes);
 app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "https://danktickets.herokuapp.com");
 		//res.header("Access-Control-Allow-Credentials", "true");
-		//res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT"); 
+		res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT"); 
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         next();
   });
