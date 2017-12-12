758c0be492ebca7b3bd08cdae57bad4a7deb7c65
Natti Katz <katzy687@gmail.com>
2017-12-12 19:55:52 +0200

    added routing active tabs on home page

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,20 +1,28 @@
-app.controller('homeCtrl',['createService','$rootScope', '$scope', '$http','$window', function(createService,$rootScope, $scope, $http, $window){
-	  function homeEventsPrep() {
-  createService.getEvents().then(function(result){
-    console.log("All events: ",result);
-    $scope.events = result;    
-  }, function(err){
-    throw (err)
-  })//GET request route
-	 }
-	 $scope.logout = function () {
-          console.log("logging out...");
-          localStorage.removeItem("user");
-          $rootScope.currentUser = null;
-          delete $http.defaults.headers.common.Authorization;
-        }
+app.controller('homeCtrl', ['createService', '$rootScope', '$scope', '$http', '$window', '$location', function (createService, $rootScope, $scope, $http, $window, $location) {
+
   this.$onInit = () => {
-   homeEventsPrep();
-   console.log("hello from hoe ctrl");
+    homeEventsPrep();
+    console.log("hello from hoe ctrl");
   }
+
+  $scope.isActive = function (route) {
+    return route === $location.path();
+  }
+
+  function homeEventsPrep() {
+    createService.getEvents().then(function (result) {
+      console.log("All events: ", result);
+      $scope.events = result;
+    }, function (err) {
+      throw (err)
+    })//GET request route
+  }
+
+  $scope.logout = function () {
+    console.log("logging out...");
+    localStorage.removeItem("user");
+    $rootScope.currentUser = null;
+    delete $http.defaults.headers.common.Authorization;
+  }
+
 }]);
