5de0e763dd9aab23cdafc1c514109f72ad33be6d
Arik92 <arik92@gmail.com>
2017-12-14 14:18:12 +0200

    more cartwork'

--- public/js/controllers/eventCtrl.js
+++ public/js/controllers/eventCtrl.js
@@ -15,21 +15,32 @@ app.controller('eventCtrl',['$scope' ,'$rootScope','$stateParams','createService
 		initEvent();   		
 		$scope.ticketCart = [];
 		$scope.ticketSum = 0;
+		$scope.ticketsToAdd = 0;
   } //initialization  
   
-  $scope.addTocart = function(ticket, numTickets) {
-	  if ((numTickets>0)&&(ticket.ticketQ>=numTicks)) {//TODO: WHOLE VALUES ONLY
-	  var cartObj = {
-		  'ticketName': ticket.ticketName,
+  $scope.addToCart = function(ticket, numTickets) {
+	  if ((numTickets>0)&&(ticket.ticketQ>=numTickets)) {//TODO: WHOLE VALUES ONLY
+	  var cartTicket = {
+		  'ticket': ticket,
 		  'quantity': numTickets
-	  }
-	  $scope.ticketCart.push(cartObj);
-	  $scope.ticketSum+=ticket.price* numTickets;
+	  }//cartObj creation
+	  $scope.ticketCart.push(cartTicket);
+	  $scope.ticketSum+=ticket.ticketPrice* numTickets;
 	  } else {
 		  alert("cannot add that many tickets!");
 	  }//else 
   }//addToCart  - cart is for the user to mess with and make his order. this doest buy or reserve them yet!
-	$scope.removeFromCart = function(index, howmany
+	$scope.updateSum = function() {
+		$scope.ticketSum = 0;
+		for (var i=0;i<$scope.ticketCart.length;i++) {
+			$scope.ticketSum+= $scope.ticketCart[i].ticketPrice*$scope.ticketCart[i].ticketQ;
+		}//for 
+	} //update sum to update any changes made to ticket quantities
+	$scope.removeFromCart = function(index) {
+		//$scope.ticketSum-=$scope.ticketCart[index].ticketPrice*$scope.ticketCart[index].ticketQ; feels DRY
+	 $scope.ticketCart.splice(index, 1);
+	 $scope.updateSum();
+	}//rrmove from cart
   $scope.buyTickets = function(numTickets) {
 	//TODO: check that the event has said number of tickets available. if it does, connect to socket and reserve tickets
 	// have a request to update the db about and reserve said tickets 
