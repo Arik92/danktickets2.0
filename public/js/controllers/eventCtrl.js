5e08411ecda9f64daae692ec49c68ab6cca6c173
Natti Katz <katzy687@gmail.com>
2017-12-17 20:56:50 +0200

    mobile started

--- public/js/controllers/eventCtrl.js
+++ public/js/controllers/eventCtrl.js
@@ -30,14 +30,16 @@ app.controller('eventCtrl',['$scope' ,'$rootScope','$stateParams','createService
 		  alert("cannot add that many tickets!");
 	  }//else 
   }//addToCart  - cart is for the user to mess with and make his order. this doest buy or reserve them yet!
-  $scope.cartPlus = function(index) {
+	
+	$scope.cartPlus = function(index) {
 	  $scope.ticketCart[index].quantity++;
   if ($scope.ticketCart[index].quantity<=$scope.ticketCart[index].ticket.ticketQ) {
 	  $scope.updateSum();
 	} else {
 		$scope.ticketCart[index].quantity--;
 	}//else 
-  }//cartplus
+	}//cartplus
+	
    $scope.cartMinus = function(index) {
 	  $scope.ticketCart[index].quantity--;
 	  if ($scope.ticketCart[index].quantity===0) {
@@ -45,13 +47,15 @@ app.controller('eventCtrl',['$scope' ,'$rootScope','$stateParams','createService
 	  } else {
 	  	  $scope.updateSum();
 	  }//else
-  }//cartMinus
+	}//cartMinus
+	
 	$scope.updateSum = function() {
 		$scope.ticketSum = 0;
 		for (var i=0;i<$scope.ticketCart.length;i++) {
 			$scope.ticketSum+= $scope.ticketCart[i].ticket.ticketPrice*$scope.ticketCart[i].quantity;
 		}//for 
 	} //update sum to update any changes made to ticket quantities
+	
 	$scope.removeFromCart = function(index) {
 		//$scope.ticketSum-=$scope.ticketCart[index].ticketPrice*$scope.ticketCart[index].ticketQ; feels DRY
 	 $scope.ticketCart.splice(index, 1);
