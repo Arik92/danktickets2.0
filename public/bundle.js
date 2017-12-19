c32e4aa37ca13156d5c8f5f83cf5eb6ea2fb2ce0
Arik92 <arik92@gmail.com>
2017-12-19 12:51:01 +0200

    todays deploy

--- public/bundle.js
+++ public/bundle.js
@@ -330,8 +330,8 @@ app.controller('createCtrl', ['createService', 'orService', 'userService', '$sco
     if (isLegit.localeCompare("ok") === 0) {
       //console.log("compared " + isLegit + " and ok. and the result is" + isLegit.localeCompare("ok"));
       if (submitPic) {
-        Upload.upload({ //'https://danktickets.herokuapp.com/events/upload'
-          url: 'http://localhost:8000/events/upload',//webAPI exposed to upload the file
+        Upload.upload({ // 'http://localhost:8000/events/upload'
+          url: 'https://danktickets.herokuapp.com/events/upload',//webAPI exposed to upload the file
           data: {
             file: submitPic,
             event: evt
@@ -581,8 +581,8 @@ app.controller('editCtrl',['createService','orService', 'userService', '$scope' 
               var submitPic = document.getElementById('fileItem').files[0];
               console.log("in submit! uploading...", submitPic);
               if (submitPic) {
-                Upload.upload({ //'https://danktickets.herokuapp.com/events/deleteAndUpload'
-                    url: 'http://localhost:8000/events/deleteAndUpload', //exposed to upload the file
+                Upload.upload({ //'https://danktickets.herokuapp.com/events/deleteAndUpload' 'http://localhost:8000/events/deleteAndUpload'
+                    url: 'https://danktickets.herokuapp.com/events/deleteAndUpload', //exposed to upload the file
                     data: {
                      file: submitPic,
                      event: $scope.event
