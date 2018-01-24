635923c8fdd92ac38a5216a2cc485e37bc6326d3
Natti Katz <katzy687@gmail.com>
2018-01-24 14:43:46 +0200

    added danktotal function

--- public/js/controllers/ticketCtrl.js
+++ public/js/controllers/ticketCtrl.js
@@ -3,24 +3,34 @@ app.controller('ticketCtrl', ['createService', '$scope', '$window', '$stateParam
 		this.$onInit = () => {
 			//localStorage.removeItem('dankCart');// PANIC button
 			$scope.params = $stateParams;
-			 var storageCart= localStorage.getItem('dankCart');
-			 console.log("storage cart from cart",storageCart);
+			var storageCart = localStorage.getItem('dankCart');
+			console.log("storage cart from cart", storageCart);
 			if (storageCart) {
-             $scope.dankCart = JSON.parse(storageCart);
-			 console.log( $scope.dankCart);
-			} 			
+				$scope.dankCart = JSON.parse(storageCart);
+				console.log($scope.dankCart);
+			}
 			//$scope.tickets = tickets;
-			//console.log($scope.params);      
+			//console.log($scope.params); 
+			getDankCartTotal();
 		}//onInit 
 		$scope.showTickets = function () {
 			console.log("Ticketss", $scope.eventTickets);
-		}		
+		}
 		$scope.remove = function (index) {
 			$scope.dankCart.splice(index, 1);
 			localStorage.setItem('dankCart', JSON.stringify($scope.dankCart));
 		}
-		$scope.clear = function() {
+		$scope.clear = function () {
 			localStorage.removeItem('dankCart');
 			console.log("removed", localstorage.getItem('dankCart'));
 		}
+
+		function getDankCartTotal() {
+			const total = $scope.dankCart.reduce((total, ticket) => {
+				return total += ticket.ticketPrice;
+			}, 0)
+
+			$scope.dankCartTotal = total;
+		}
+
 	}]);
