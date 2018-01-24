b1b0360486665444027c757112955f8d69e2fa2b
Natti Katz <katzy687@gmail.com>
2018-01-24 21:27:29 +0200

    added moment js, made timeService, and combined directives

--- public/js/directives/filteredEventDirective.js
+++ public/js/directives/filteredEventDirective.js
@@ -1,15 +1,15 @@
-app.directive('filteredEventDirective', function($compile) {
+app.directive('filteredEventDirective', function ($compile) {
   return {
-      restrict: 'E',
-      scope: {
-          myevents: "=",
-          mylimit: "=",
-		  myfilter: "="
-      },
-      controller: ($scope) => {
-        console.log('yo from filtered events directive');
-      },
-      templateUrl:'filteredEventDirective.html',
-      replace: true
+    restrict: 'E',
+    scope: {
+      myevents: "=",
+      mylimit: "=",
+      myfilter: "="
+    },
+    controller: ($scope) => {
+      console.log('yo from filtered events directive');
+    },
+    templateUrl: 'filteredEventDirective.html',
+    replace: true
   }
 });
