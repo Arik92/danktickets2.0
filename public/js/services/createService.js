96210bb5acf99b46f88012f4de68cc5a230e3d97
Arik92 <arik92@gmail.com>
2018-01-27 21:16:05 +0200

    testing

--- public/js/services/createService.js
+++ public/js/services/createService.js
@@ -70,7 +70,7 @@ app.factory('createService', function ($http) {
 
   var generalSearch = function (searchQuery) {
 	  //searchQuery = '/'+searchQuery+'/'+i;
-	  //console.log('service patt', searchQuery);
+	  console.log('service patt', searchQuery);
     return $http.get('/events/generalSearch/' + searchQuery).then(function (result) {
       return result.data;
     }, function (error) {
