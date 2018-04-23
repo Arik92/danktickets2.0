2156b758f02bb7ae6c97566dba1e5865af40cc5b
Arik92 <arik92@gmail.com>
2018-04-23 14:28:53 +0300

    aaaarg

--- public/js/app.js
+++ public/js/app.js
@@ -215,7 +215,7 @@ app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', functio
 
 app.run([ 'authService', '$rootScope', function (authService, $rootScope) {
   var user = JSON.parse(localStorage.getItem("user"));
-  console.log($rootScope);
+  console.log($rootScope.Scope);
   if ($rootScope.userDetails) {
 	  console.log("I see userdetails exists");
 	  $rootScope.currentUser = $rootScope.userDetails.id;
